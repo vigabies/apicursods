30a5c122d230e653e67baf4b37dea8a07e122873
unknown <vigabiesdivision@gmail.com>
2022-06-06 08:19:56 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/gabi/apicursods/ApicursodsApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabi.apicursods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicursodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicursodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabi/apicursods/ApicursodsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabi.apicursods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicursodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
